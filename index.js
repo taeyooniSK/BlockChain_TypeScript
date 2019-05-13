e9e30db62a9c7efbbbd3a862c5323f269e58c651
Tae-yoon <yeotaeyoon@gmail.com>
2019-05-14 01:09:22 +0900

    TypeScript checks if the number of arguments that defined in function and tells you what is missing

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("Hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Taeyoon", age = 29, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello");
+const name = "Taeyoon",
+age = 29,
+gender = "male";
+
+
+const sayHi = (name, age, gender?) => {  // if '?' is put behind parmeter, that means the parameter is optional 
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi(name,age); // TypeScript tells you what argument is missing
+
+
+export {};
