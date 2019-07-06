33cf18c53230f9983789c1a6a2541a6037ac5538
Tae-yoon <yeotaeyoon@gmail.com>
2019-07-06 23:52:14 +0900

    usage of class in typscript and make blockchain

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,36 @@
 "use strict";
+// class Human { 
+//     public name: string;
+//     public age : number;
+//     public gender : string;
+//     constructor(name: string, age: number, gender?: string){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "Taeyoon",
-    age: 29,
-    gender: "male"
-};
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`; // string을 리턴하므로 유효함
-};
-console.log(sayHi(person));
+// const taeyoon = new Human("Taeyoon", 28, "male");
+// const sayHi = (person: Human):string => {   // ':string'은 함수가 string을 리턴할 것이라고 타입을 설정해주는 의미
+//     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;     // string을 리턴하므로 유효함
+// }
+// console.log(sayHi(taeyoon)); 
+const CryptoJS = require("crypto-js");
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+Block.calclulateBlockHash = (// static methods can be used even before when you instantiate class Block
+index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+const genesisBlock = new Block(0, "20202020222", "", "Hello world!", 123456);
+let blockChain = [genesisBlock];
+const getBlockChain = () => blockChain;
+const getLatestBlock = () => blockChain[blockChain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+console.log(blockChain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,65 @@
-const person = {
-    name: "Taeyoon",
-    age: 29,
-    gender: "male"
+// class Human { 
+//     public name: string;
+//     public age : number;
+//     public gender : string;
+//     constructor(name: string, age: number, gender?: string){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+// }
+
+// const taeyoon = new Human("Taeyoon", 28, "male");
+
+// const sayHi = (person: Human):string => {   // ':string'은 함수가 string을 리턴할 것이라고 타입을 설정해주는 의미
+//     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;     // string을 리턴하므로 유효함
+// }
+
+// console.log(sayHi(taeyoon)); 
+
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calclulateBlockHash = ( // static methods can be used even before when you instantiate class Block
+        index: number, 
+        previousHash: string, 
+        timestamp: number, 
+        data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+
+    constructor(
+        index: number, 
+        hash: string, 
+        previousHash: string, 
+        data: string, 
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
-}
 
-const sayHi = (person: Human):string => {   // ':string'은 함수가 string을 리턴할 것이라고 타입을 설정해주는 의미
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;     // string을 리턴하므로 유효함
-}
 
-console.log(sayHi(person)); 
+const genesisBlock:Block = new Block(0, "20202020222", "", "Hello world!", 123456);
 
+let blockChain:Block[] = [genesisBlock];
+
+const getBlockChain = () : Block[] => blockChain;
+
+const getLatestBlock = () : Block => blockChain[blockChain.length-1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+console.log(blockChain);
 
 export {};
