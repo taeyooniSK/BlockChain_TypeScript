6760a62e941ed9293a65c450b9eeefa36093e710
Tae-yoon <yeotaeyoon@gmail.com>
2019-07-06 23:28:05 +0900

    Interface type for JS object

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,12 @@
 "use strict";
-// const sayHi = (name:string, age:number, gender:string):void => {   // when you don't want the function to return anything, you can put ":void" 비었다는 뜻
-//     return `Hello ${name}, you are ${age}, you are a ${gender}`;     //이 경우엔 error가 뜸 (함수에서 리턴되는 값이 없다고 type을 voide로 설정해놨는데 return된 값이 나오므로)
-// }
 Object.defineProperty(exports, "__esModule", { value: true });
-// sayHi("Taeyoon", 29, "male"); 
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`; // string을 리턴하므로 유효함
+const person = {
+    name: "Taeyoon",
+    age: 29,
+    gender: "male"
 };
-console.log(sayHi("Taeyoon", 29, "male"));
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`; // string을 리턴하므로 유효함
+};
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
